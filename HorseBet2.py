32d7d370b174355e5c17a11d8c69157904253844
Fetu97 <Ferhat-Arslan97@hotmail.com>
2022-01-30 13:48:49 +0200

    Initial commit

---
+++ HorseBet2.py
@@ -0,0 +1,420 @@
+# Description.
+print("Made by Fetu / 30.1.2022") 
+print("CnR, HorseBet Gamble Tool\n") 
+
+# Main loop.
+mainLoop = ""
+while mainLoop != "n" or "N":
+
+    for i in range(5):
+        file = open("list.txt", "r")
+        betName = input("Enter the name:\n\n")
+        print("***")
+        
+# List starts here.
+        if betName == "a step ahead":
+            list = file.readlines()
+            list = list[2:7]
+            for x in list:
+                print(x)
+
+        if betName == "a whole lotta luck":
+            list = file.readlines()
+            list = list[14:19]
+            for x in list:
+                print(x)
+
+        if betName == "al caponey":
+            list = file.readlines()
+            list = list[26:39]
+            for x in list:
+                print(x)
+
+        if betName == "alphabet seeker":
+            list = file.readlines()
+            list = list[63:76]
+            for x in list:
+                print(x)
+
+        if betName == "america's most wanted":
+            list = file.readlines()
+            list = list[82:93]
+            for x in list:
+                print(x)
+
+        if betName == "atswhatimtalknbout":
+            list = file.readlines()
+            list = list[99:108]
+            for x in list:
+                print(x)
+
+        if betName == "banned regular":
+            list = file.readlines()
+            list = list[114:127]
+            for x in list:
+                print(x)
+
+        if betName == "blazin' saddles":
+            list = file.readlines()
+            list = list[133:138]
+            for x in list:
+                print(x)
+
+        if betName == "bonnie":
+            list = file.readlines()
+            list = list[144:149]
+            for x in list:
+                print(x)
+
+        if betName == "bubba":
+            list = file.readlines()
+            list = list[155:164]
+            for x in list:
+                print(x)
+
+        if betName == "buddy gil":
+            list = file.readlines()
+            list = list[170:175]
+            for x in list:
+                print(x)
+
+        if betName == "charge":
+            list = file.readlines()
+            list = list[181:186]
+            for x in list:
+                print(x)
+
+        if betName == "clyde":
+            list = file.readlines()
+            list = list[192:197]
+            for x in list:
+                print(x)
+
+        if betName == "crowned king":
+            list = file.readlines()
+            list = list[203:221]
+            for x in list:
+                print(x)
+
+        if betName == "dealer's choice":
+            list = file.readlines()
+            list = list[228:241]
+            for x in list:
+                print(x)
+
+        if betName == "deputy":
+            list = file.readlines()
+            list = list[247:256]
+            for x in list:
+                print(x)
+
+        if betName == "devil's ride":
+            list = file.readlines()
+            list = list[262:277]
+            for x in list:
+                print(x)
+
+        if betName == "domestic dispute":
+            list = file.readlines()
+            list = list[283:292]
+            for x in list:
+                print(x)
+
+        if betName == "dream queen":
+            list = file.readlines()
+            list = list[298:307]
+            for x in list:
+                print(x)
+
+        if betName == "envy":
+            list = file.readlines()
+            list = list[313:318]
+            for x in list:
+                print(x)
+
+        if betName == "eye of the tiger":
+            list = file.readlines()
+            list = list[324:341]
+            for x in list:
+                print(x)
+
+        if betName == "flash of lightning":
+            list = file.readlines()
+            list = list[347:359]
+            for x in list:
+                print(x)
+
+        if betName == "fuego":
+            list = file.readlines()
+            list = list[365:382]
+            for x in list:
+                print(x)
+
+        if betName == "funny cide":
+            list = file.readlines()
+            list = list[388:396]
+            for x in list:
+                print(x)
+
+        if betName == "gift from god":
+            list = file.readlines()
+            list = list[402:417]
+            for x in list:
+                print(x)
+
+        if betName == "goinwest":
+            list = file.readlines()
+            list = list[423:429]
+            for x in list:
+                print(x)
+
+        if betName == "gold rush":
+            list = file.readlines()
+            list = list[434:445]
+            for x in list:
+                print(x)
+        
+        if betName == "homewrecker":
+            list = file.readlines()
+            list = list[451:456]
+            for x in list:
+                print(x)
+
+        if betName == "hot sauce":
+            list = file.readlines()
+            list = list[462:478]
+            for x in list:
+                print(x)
+
+        if betName == "irish attitude":
+            list = file.readlines()
+            list = list[484:497]
+            for x in list:
+                print(x)
+
+        if betName == "irresponsible king":
+            list = file.readlines()
+            list = list[502:530]
+            for x in list:
+                print(x)
+
+        if betName == "jet jockey":
+            list = file.readlines()
+            list = list[536:549]
+            for x in list:
+                print(x)
+
+        if betName == "kentucky king":
+            list = file.readlines()
+            list = list[555:560]
+            for x in list:
+                print(x)
+
+        if betName == "kodak moment":
+            list = file.readlines()
+            list = list[566:581]
+            for x in list:
+                print(x)
+
+        if betName == "kuhl 'n' clever":
+            list = file.readlines()
+            list = list[587:603]
+            for x in list:
+                print(x)
+
+        if betName == "lion tamer":
+            list = file.readlines()
+            list = list[609:614]
+            for x in list:
+                print(x)
+
+        if betName == "lucky star":
+            list = file.readlines()
+            list = list[620:625]
+            for x in list:
+                print(x)
+
+        if betName == "magic carpet ride":
+            list = file.readlines()
+            list = list[631:640]
+            for x in list:
+                print(x)
+
+        if betName == "man among men":
+            list = file.readlines()
+            list = list[646:659]
+            for x in list:
+                print(x)
+
+        if betName == "me my mine":
+            list = file.readlines()
+            list = list[665:674]
+            for x in list:
+                print(x)
+
+        if betName == "midas eyes":
+            list = file.readlines()
+            list = list[681:688]
+            for x in list:
+                print(x)
+
+        if betName == "midnight rider":
+            list = file.readlines()
+            list = list[694:706]
+            for x in list:
+                print(x)
+
+        if betName == "money in the bank":
+            list = file.readlines()
+            list = list[712:717]
+            for x in list:
+                print(x)
+
+        if betName == "nothing to loose":
+            list = file.readlines()
+            list = list[723:732]
+            for x in list:
+                print(x)
+
+        if betName == "old faithfull":
+            list = file.readlines()
+            list = list[738:756]
+            for x in list:
+                print(x)
+
+        if betName == "peace rules":
+            list = file.readlines()
+            list = list[762:778]
+            for x in list:
+                print(x)
+
+        if betName == "pretty funny":
+            list = file.readlines()
+            list = list[784:797]
+            for x in list:
+                print(x)
+
+        if betName == "private gold":
+            list = file.readlines()
+            list = list[803:808]
+            for x in list:
+                print(x)
+
+        if betName == "raider":
+            list = file.readlines()
+            list = list[814:819]
+            for x in list:
+                print(x)
+
+        if betName == "riding miss daisy":
+            list = file.readlines()
+            list = list[825:830]
+            for x in list:
+                print(x)
+
+        if betName == "saintly look":
+            list = file.readlines()
+            list = list[836:848]
+            for x in list:
+                print(x)
+
+        if betName == "sea this biscuit":
+            list = file.readlines()
+            list = list[854:867]
+            for x in list:
+                print(x)
+
+        if betName == "second in command":
+            list = file.readlines()
+            list = list[873:890]
+            for x in list:
+                print(x)
+
+        if betName == "secret weapon":
+            list = file.readlines()
+            list = list[896:901]
+            for x in list:
+                print(x)
+
+        if betName == "shake n bake":
+            list = file.readlines()
+            list = list[907:912]
+            for x in list:
+                print(x)
+
+        if betName == "silver bullet":
+            list = file.readlines()
+            list = list[918:939]
+            for x in list:
+                print(x)
+
+        if betName == "standard setter":
+            list = file.readlines()
+            list = list[945:950]
+            for x in list:
+                print(x)
+
+        if betName == "stardust":
+            list = file.readlines()
+            list = list[956:961]
+            for x in list:
+                print(x)
+
+        if betName == "surething":
+            list = file.readlines()
+            list = list[992:997]
+            for x in list:
+                print(x)
+
+        if betName == "the real slim shady":
+            list = file.readlines()
+            list = list[1003:1015]
+            for x in list:
+                print(x)
+
+        if betName == "too easy":
+            list = file.readlines()
+            list = list[1021:1026]
+            for x in list:
+                print(x)
+
+        if betName == "trust n luck":
+            list = file.readlines()
+            list = list[1032:1045]
+            for x in list:
+                print(x)
+
+        if betName == "unchallenged win":
+            list = file.readlines()
+            list = list[1051:1061]
+            for x in list:
+                print(x)
+
+        if betName == "wal-mart shopper":
+            list = file.readlines()
+            list = list[1066:1075]
+            for x in list:
+                print(x)
+
+        if betName == "white lightning":
+            list = file.readlines()
+            list = list[1081:1089]
+            for x in list:
+                print(x)
+
+        if betName == "victory moon":
+            list = file.readlines()
+            list = list[1095:1100]
+            for x in list:
+                print(x)
+# List ends here...
+
+# Asks you do u want to continue the program or not.                    
+    selectKey = input("\nDo you want to continue? (Y/N): ")
+
+    if (selectKey == "n") or (selectKey == "N"):
+        print("Program Ends, Press Enter...")
+        break
+
+    elif (selectKey == "y") or (selectKey == "Y"):
+        print("Program Continues...\n")
